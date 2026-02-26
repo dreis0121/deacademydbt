88504b31adb663276f720f14bd104d12413c7612
Derick <reddr81@hotmail.com>
2026-02-26 21:26:22 +0000

    updates to yaml and marts tables

--- models/marts/marketing/dim_customers.sql
+++ models/marts/marketing/dim_customers.sql
@@ -33,9 +33,8 @@ final as (
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
-        coalesce (customer_orders.number_of_orders, 0),
+        coalesce(customer_orders.number_of_orders, 0) AS number_of_orders,
         customer_orders.lifetime_value
-        as number_of_orders
 
     from customers
 
