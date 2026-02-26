ebc54399efbfd20a0b210ecb56fda0f55c0be9e4
Derick <reddr81@hotmail.com>
2026-02-26 20:10:04 +0000

    created initial framework for jaffle shop with dim and fact tables, and folder structures. Also updated projects.yaml

---
+++ models/staging/stripe/stg_stripe_payments.sql
@@ -0,0 +1,7 @@
+select
+    id AS payment_id,
+    orderid AS order_id,
+    paymentmethod AS payment_method,
+    status,
+    amount /100  as amount -- stored in cents by default, this stores it as dollars
+from raw.stripe.payment
